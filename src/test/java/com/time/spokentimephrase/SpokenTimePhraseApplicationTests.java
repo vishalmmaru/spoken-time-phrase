51ba3e2f90e010fdc3ebac2f7fce0ef631d5ad4c
vishalmmaru <vishal.m.maru@gmail.com>
2026-09-19 17:23:03 +0530

    initial commit for adding spring and gradle related files

---
+++ src/test/java/com/time/spokentimephrase/SpokenTimePhraseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time.spokentimephrase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpokenTimePhraseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
